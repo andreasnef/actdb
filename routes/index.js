efae62aceb13e19bb45833861d8643a8a8a373fd
malenage <malenage@gmail.com>
2018-06-14 14:58:44 +0200

    remove old file

--- routes/index.js
+++ routes/index.js
@@ -417,7 +417,6 @@ router.get('/report', csrfProtection, function(req, res){
                 mongoCommon.getRelatedProfiles(result, 'mps'),
                 mongoCommon.getRelatedProfiles(result, 'sites')
             ]).then((results)=> {
-                    console.log("results"+JSON.stringify(results[3]));
                     res.render('report', {    
                         title : "Report on "+result[0].name.en,
                         profile : result,
