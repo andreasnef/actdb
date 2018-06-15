9ff11519f5c77e9edafe9f14a42a8f26570e019d
malenage <malenage@gmail.com>
2018-06-16 00:35:26 +0200

    port config

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ app.use(function(err, req, res, next) {
 });
 
 //use in PRO
-// var port = process.env.PORT || 3000;
-// app.listen(port, "0.0.0.0", function() {
-//   console.log("Listening on Port 3000");
-// });
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+});
 
 var RateLimit = require('express-rate-limit');
  
